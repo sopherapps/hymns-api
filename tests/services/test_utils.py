b4a28bb53498adccc050ac3f036788cbda4a4d07
Martin <sales@sopherapps.com>
2023-02-16 19:56:50 +0300

    Add tests for more utils

--- tests/services/test_utils.py
+++ tests/services/test_utils.py
@@ -1,9 +1,11 @@
 """Tests for all utility functions common across services"""
+import json
 import os
 
-from services.utils import (
-    get_store_path,
-)
+import funml as ml
+
+from services.hymns.models import MusicalNote
+from services.utils import get_store_path, record_to_json, note_to_str
 
 _ROOT_FOLDER_PATH = os.path.dirname(
     os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -24,9 +26,50 @@ def test_get_store_path():
 
 def test_record_to_json():
     """record_to_json converts a record to JSON string"""
-    pass
+
+    @ml.record
+    class Color:
+        r: int
+        g: int
+        b: int
+
+    test_data = [
+        (Color(r=9, g=90, b=7), json.dumps({"r": 9, "g": 90, "b": 7})),
+        (Color(r=19, g=100, b=17), json.dumps({"r": 19, "g": 100, "b": 17})),
+        (Color(r=4, g=30, b=45), json.dumps({"r": 4, "g": 30, "b": 45})),
+    ]
+
+    for item, expected in test_data:
+        assert record_to_json(item) == expected
 
 
 def test_note_to_str():
     """note_to_str converts a MusicalNote into a string"""
-    pass
+    test_data = [
+        (MusicalNote.C_MAJOR, "C"),
+        (MusicalNote.C_MINOR, "Cm"),
+        (MusicalNote.C_SHARP_MAJOR, "C#"),
+        (MusicalNote.C_SHARP_MINOR, "C#m"),
+        (MusicalNote.D_MAJOR, "D"),
+        (MusicalNote.D_SHARP_MAJOR, "D#"),
+        (MusicalNote.D_SHARP_MINOR, "D#m"),
+        (MusicalNote.E_MAJOR, "E"),
+        (MusicalNote.E_MINOR, "Em"),
+        (MusicalNote.F_MAJOR, "F"),
+        (MusicalNote.F_MINOR, "Fm"),
+        (MusicalNote.F_SHARP_MAJOR, "F#"),
+        (MusicalNote.F_SHARP_MINOR, "F#m"),
+        (MusicalNote.G_MAJOR, "G"),
+        (MusicalNote.G_MINOR, "Gm"),
+        (MusicalNote.G_SHARP_MAJOR, "G#"),
+        (MusicalNote.G_SHARP_MINOR, "G#m"),
+        (MusicalNote.A_MAJOR, "A"),
+        (MusicalNote.A_MINOR, "Am"),
+        (MusicalNote.A_SHARP_MAJOR, "A#"),
+        (MusicalNote.A_SHARP_MINOR, "A#m"),
+        (MusicalNote.B_MAJOR, "B"),
+        (MusicalNote.B_MINOR, "Bm"),
+    ]
+
+    for item, expected in test_data:
+        assert note_to_str(item) == expected
